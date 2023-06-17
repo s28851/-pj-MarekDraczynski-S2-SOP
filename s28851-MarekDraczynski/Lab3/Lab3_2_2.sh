19615fa7b9c18ee831520eb6d50c445954cc746f
Marek <md@mediman.pl>
2023-06-17 06:19:57 +0200

    ;

--- s28851-MarekDraczynski/Lab3/Lab3_2_2.sh
+++ s28851-MarekDraczynski/Lab3/Lab3_2_2.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Napisz skrypt, który będzie wypisywał liczby od 10 do 1 co 2
+# Napisz skrypt, który będzie wypisywał wszystkie argumenty wywołania skryptu 
+# (pamiętaj o tym, że argument może w sobie zawierać jakieś spacje) napisane w cudzysłowach.
 
-for (( i = 10 ; i > -1 ; i -= 2 )); do echo $i; done
-for i in $(seq 10 -2 0); do echo $i; done
-for i in {10..0..-2}; do echo $i ; done
-
+for i in $*; do echo $i; done;
